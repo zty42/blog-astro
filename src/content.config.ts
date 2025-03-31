9d87d8fa01ff6c6df1c471695b8e61d3ed5e63bf
zty <zty.dev@outlook.com>
2025-03-31 17:39:21 +0800

    新增多篇博客文章及更新内容配置

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,11 +7,9 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
+		date: z.string(),
 		heroImage: z.string().optional(),
+		tags: z.array(z.string()).optional()
 	}),
 });
 
