8ad3a00df4747532a4feff8d0f2c103d92f9f69c
zty <zty.dev@outlook.com>
2025-04-01 11:29:54 +0800

    更新网站描述为“乱七八糟 瞎写”

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "zty's Blog";
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = '乱七八糟 瞎写';
