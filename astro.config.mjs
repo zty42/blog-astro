7acf75bfd1aa2d862e810700e954a5ed3c1a2eed
zty <zty.dev@outlook.com>
2025-04-02 15:01:04 +0800

    更新站点URL为'https://zty.im'

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://zty.im',
   integrations: [mdx(), sitemap(), react()],
   markdown: {
     shikiConfig: {
