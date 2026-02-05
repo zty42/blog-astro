cd2a4bb4a83429a1267362bf1c150ac2849c972f
zty <tianyuanzhang66@gmail.com>
2026-02-05 17:45:10 +0800

    重构项目结构，更新包名，添加新的组件和图标，优化样式，增强可访问性

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "zty's Blog";
+export const SITE_TITLE = "ZTY's Blog";
 export const SITE_DESCRIPTION = '乱七八糟 瞎写';
