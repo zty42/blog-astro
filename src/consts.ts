b621520b57b20be5ea8eb0d205535c60f773d4b6
zty <tianyuanzhang66@gmail.com>
2026-02-06 09:27:38 +0800

    Improve homepage snippet metadata and intro

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "ZTY's Blog";
-export const SITE_DESCRIPTION = '乱七八糟 瞎写';
+export const SITE_DESCRIPTION = 'zty 的个人博客，记录技术、生活与思考。';
